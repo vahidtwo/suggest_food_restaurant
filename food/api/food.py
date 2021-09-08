62e55fa3cd9f33e5500570c241c5d4a1b53894b1
vahid <vahidtwo@gmail.com>
2021-09-08 05:09:24 +0430

    fix: unnecessary if deleted

--- food/api/food.py
+++ food/api/food.py
@@ -30,8 +30,6 @@ class SuggestFoodApi(APIView):
     def get(self, request):
         tag_ids = request.query_params.get('tag_ids')
         tag_ids = list(map(int, tag_ids.split(',')))
-        if not isinstance(tag_ids, list):
-            return Response('tag_ids must be a list of integer', status=400)
         tags = Tag.objects.filter(pk__in=tag_ids)
         self.queryset = self.queryset.annotate(sub_tag_count=Count('tags', filter=Q(
             tags__in=tags))).order_by('-sub_tag_count')
